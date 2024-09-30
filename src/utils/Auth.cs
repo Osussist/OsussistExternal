1afdb648d0e675b29048fe21691f28a2de73ef04
Takkeshi <prosaso12133@gmail.com>
2024-09-30 16:26:30 -0600

    Remove unused using directives in Auth.cs
    Cleaned up unused using directives for System, System.Collections.Generic, System.Linq, System.Text, and System.Threading.Tasks in Auth.cs. This reduces unnecessary dependencies and improves code readability and maintainability. No changes were made to the namespace declaration or the structure/content of the Auth class.

--- src/utils/Auth.cs
+++ src/utils/Auth.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Osussist.src.utils
+﻿namespace Osussist.src.utils
 {
     public class Auth
     {
