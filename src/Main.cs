ff379619ca9f6881f8b039c0f984931a6ba43b33
LUXTACO <tacomastabusiness@gmail.com>
2024-10-11 10:23:07 -0600

    Add admin privilege check and warning message
    Introduce a check to determine if the program is running with administrator privileges. If not, display a warning message suggesting to run the program as an administrator for proper functionality. Added `using System.Security.Principal;` directive to support this feature. Main logic and structure remain unchanged.

--- src/Main.cs
+++ src/Main.cs
@@ -7,131 +7,138 @@ using Osussist.src.osu.helpers;
 using Osussist.src.utils;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
+using System.Security.Principal;
 using System.Windows.Forms;
 using static Osussist.src.osu.helpers.OsuProcess;
 
 namespace Osussist
 {
-    class Program
-    {
-        #region NativeImports
-        [DllImport("kernel32.dll")]
-        static extern IntPtr GetConsoleWindow();
+	class Program
+	{
+		#region NativeImports
+		[DllImport("kernel32.dll")]
+		static extern IntPtr GetConsoleWindow();
 
-        [DllImport("user32.dll")]
-        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+		[DllImport("user32.dll")]
+		static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
-        const int SW_HIDE = 0;
-        const int SW_SHOW = 5;
-        #endregion
+		const int SW_HIDE = 0;
+		const int SW_SHOW = 5;
+		#endregion
 
-        [STAThread]
-        static void Main(string[] args)
-        {
-            Logger logger = new Logger(true, "osussist.log", (int)LogLevels.INFO);
+		[STAThread]
+		static void Main(string[] args)
+		{
+			Logger logger = new Logger(true, "osussist.log", (int)LogLevels.INFO);
+			bool isAdministrator = new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
 
-            AppDomain currentDomain = default(AppDomain);
-            currentDomain = AppDomain.CurrentDomain;
-            currentDomain.UnhandledException += GlobalUnhandledExceptionHandler;
-            Application.ThreadException += GlobalThreadExceptionHandler;
+			if (!isAdministrator)
+			{
+				MessageBox.Show("We suggest you run this program as administrator, not doing so will prevent the spoofer from working!", "Warning, program not running as admin!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
 
-            try
-            {
-                var consoleHandle = GetConsoleWindow();
+			AppDomain currentDomain = default(AppDomain);
+			currentDomain = AppDomain.CurrentDomain;
+			currentDomain.UnhandledException += GlobalUnhandledExceptionHandler;
+			Application.ThreadException += GlobalThreadExceptionHandler;
 
-                if (args.Length > 0)
-                {
-                    if (args.Contains("--showconsole") || args.Contains("-sc"))
-                        ShowWindow(consoleHandle, SW_SHOW);
-                    else
-                        ShowWindow(consoleHandle, SW_HIDE);
+			try
+			{
+				var consoleHandle = GetConsoleWindow();
 
-                    if (args.Contains("--debug") || args.Contains("-dbg"))
-                        logger.UpdateLogLevel((int)LogLevels.DEBUG);
+				if (args.Length > 0)
+				{
+					if (args.Contains("--showconsole") || args.Contains("-sc"))
+						ShowWindow(consoleHandle, SW_SHOW);
+					else
+						ShowWindow(consoleHandle, SW_HIDE);
 
-                    logger.Info("Main", "Starting Osussist...");
-                }
-                else
-                {
-                    ShowWindow(consoleHandle, SW_HIDE);
-                }
+					if (args.Contains("--debug") || args.Contains("-dbg"))
+						logger.UpdateLogLevel((int)LogLevels.DEBUG);
 
-                Config config = new Config();
-                if (!config.Load("config.json"))
-                    config.Save("config.json");
+					logger.Info("Main", "Starting Osussist...");
+				}
+				else
+				{
+					ShowWindow(consoleHandle, SW_HIDE);
+				}
 
-                Thread guiThread = new Thread(new ThreadStart(RenderGUI));
-                guiThread.SetApartmentState(ApartmentState.STA);
-                guiThread.Start();
-                logger.Info("Main", "GUI has been initialized successfully");
+				Config config = new Config();
+				if (!config.Load("config.json"))
+					config.Save("config.json");
 
-                OsuSDK SDK = new OsuSDK("osu!", true);
-                logger.Info("Main", "SDK has been initialized successfully");
+				Thread guiThread = new Thread(new ThreadStart(RenderGUI));
+				guiThread.SetApartmentState(ApartmentState.STA);
+				guiThread.Start();
+				logger.Info("Main", "GUI has been initialized successfully");
 
-                Relax relax = new Relax(SDK);
-                Aimbot aimbot = new Aimbot(SDK);
-                logger.Info("Main", "Cheats have been initialized successfully");
+				OsuSDK SDK = new OsuSDK("osu!", true);
+				logger.Info("Main", "SDK has been initialized successfully");
 
-                OsuMonitor monitor = new OsuMonitor();
-                monitor.StartMonitoring();
-                AppDomain.CurrentDomain.ProcessExit += (s, e) =>
-                {
-                    monitor.StopMonitoring();
-                };
-                logger.Info("Main", "Monitoring has been initialized successfully");
+				Relax relax = new Relax(SDK);
+				Aimbot aimbot = new Aimbot(SDK);
+				logger.Info("Main", "Cheats have been initialized successfully");
 
-                MainLoop(SDK, aimbot, relax);
-            }
-            catch (Exception e)
-            {
-                logger.Error("Main", e.Message);
-            }
-        }
+				OsuMonitor monitor = new OsuMonitor();
+				monitor.StartMonitoring();
+				AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+				{
+					monitor.StopMonitoring();
+				};
+				logger.Info("Main", "Monitoring has been initialized successfully");
 
-        public static void RenderGUI()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainGUI());
-            Environment.Exit(0);
-        }
+				MainLoop(SDK, aimbot, relax);
+			}
+			catch (Exception e)
+			{
+				logger.Error("Main", e.Message);
+			}
+		}
 
-        public static void MainLoop(OsuSDK SDK, Aimbot aimbotModule, Relax relaxModule)
-        {
-            try
-            {
-                Thread aimbotThread = new Thread(new ThreadStart(aimbotModule.Loop));
-                aimbotThread.Start();
+		public static void RenderGUI()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainGUI());
+			Environment.Exit(0);
+		}
 
-                if (SDK.OsuManager.ProcessManager.ClientType == ClientTypes.Stable)
-                {
-                    Thread relaxThread = new Thread(new ThreadStart(relaxModule.Loop));
-                    relaxThread.Start();
-                }
-            }
-            catch (Exception e)
-            {
-                Logger logger = Logger.LoggingInstance;
-                logger.Error("Main", e.Message);
-            }
-        }
+		public static void MainLoop(OsuSDK SDK, Aimbot aimbotModule, Relax relaxModule)
+		{
+			try
+			{
+				Thread aimbotThread = new Thread(new ThreadStart(aimbotModule.Loop));
+				aimbotThread.Start();
 
-        private static void GlobalUnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
-        {
-            Exception ex = default(Exception);
-            ex = (Exception)e.ExceptionObject;
-            Logger logger = Logger.LoggingInstance;
-            logger.Error("Main", $"{ex.Message} on line {ex.StackTrace}");
-            MessageBox.Show($"Fatal error occurred: {ex.Message}", "Error, please report this to the developer!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-        }
+				if (SDK.OsuManager.ProcessManager.ClientType == ClientTypes.Stable)
+				{
+					Thread relaxThread = new Thread(new ThreadStart(relaxModule.Loop));
+					relaxThread.Start();
+				}
+			}
+			catch (Exception e)
+			{
+				Logger logger = Logger.LoggingInstance;
+				logger.Error("Main", e.Message);
+			}
+		}
 
-        private static void GlobalThreadExceptionHandler(object sender, ThreadExceptionEventArgs e)
-        {
-            Exception ex = default(Exception);
-            ex = e.Exception;
-            Logger logger = Logger.LoggingInstance;
-            logger.Error("Main", $"{ex.Message} on line {ex.StackTrace}");
-            MessageBox.Show($"Fatal error occurred: {ex.Message}", "Error, please report this to the developer!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-        }
-    }
+		private static void GlobalUnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+		{
+			Exception ex = default(Exception);
+			ex = (Exception)e.ExceptionObject;
+			Logger logger = Logger.LoggingInstance;
+			logger.Error("Main", $"{ex.Message} on line {ex.StackTrace}");
+			MessageBox.Show($"Fatal error occurred: {ex.Message}", "Error, please report this to the developer!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		}
+
+		private static void GlobalThreadExceptionHandler(object sender, ThreadExceptionEventArgs e)
+		{
+			Exception ex = default(Exception);
+			ex = e.Exception;
+			Logger logger = Logger.LoggingInstance;
+			logger.Error("Main", $"{ex.Message} on line {ex.StackTrace}");
+			MessageBox.Show($"Fatal error occurred: {ex.Message}", "Error, please report this to the developer!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		}
+	}
 }
